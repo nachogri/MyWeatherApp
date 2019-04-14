62ad27b2400b53f5462cadf9b1d8d17365cc6213
NachoGri <nachogri@gmail.com>
2019-04-14 12:40:03 -0300

    Update ConditionsController.cs

--- MyWeatherAPI/MyWeatherAPI/Controllers/ConditionsController.cs
+++ MyWeatherAPI/MyWeatherAPI/Controllers/ConditionsController.cs
@@ -1,10 +1,6 @@
 ﻿using MyWeatherAPI.Models;
 using MyWeatherAPI.Repositories;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
 using System.Web.Http;
 using System.Web.Script.Serialization;
 
